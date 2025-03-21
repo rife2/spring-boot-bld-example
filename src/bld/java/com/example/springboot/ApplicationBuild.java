9dd02bcecc94f9bde30115730f10abf15e101b81
Erik C. Thauvin <erik@thauvin.net>
2025-03-20 21:11:07 -0700

    Bump to Spring Boot version 3.4.4

--- src/bld/java/com/example/springboot/ApplicationBuild.java
+++ src/bld/java/com/example/springboot/ApplicationBuild.java
@@ -24,16 +24,16 @@ public class ApplicationBuild extends WebProject {
         repositories = List.of(MAVEN_CENTRAL);
 
         scope(compile)
-                .include(dependency("org.springframework.boot:spring-boot-starter:3.4.3"))
-                .include(dependency("org.springframework.boot:spring-boot-starter-actuator:3.4.3"))
-                .include(dependency("org.springframework.boot:spring-boot-starter-web:3.4.3"))
+                .include(dependency("org.springframework.boot:spring-boot-starter:3.4.4"))
+                .include(dependency("org.springframework.boot:spring-boot-starter-actuator:3.4.4"))
+                .include(dependency("org.springframework.boot:spring-boot-starter-web:3.4.4"))
                 .include(dependency("org.mockito:mockito-core:5.16.1"));
         scope(test)
-                .include(dependency("org.springframework.boot:spring-boot-starter-test:3.4.3"))
+                .include(dependency("org.springframework.boot:spring-boot-starter-test:3.4.4"))
                 .include(dependency("org.junit.jupiter:junit-jupiter:5.12.1"))
                 .include(dependency("org.junit.platform:junit-platform-console-standalone:1.12.1"));
         scope(standalone)
-                .include(dependency("org.springframework.boot:spring-boot-loader:3.4.3"));
+                .include(dependency("org.springframework.boot:spring-boot-loader:3.4.4"));
 
         testOperation().javaOptions(List.of("-XX:+EnableDynamicAgentLoading"))
                 .javaOptions().enableNativeAccess("ALL-UNNAMED");
