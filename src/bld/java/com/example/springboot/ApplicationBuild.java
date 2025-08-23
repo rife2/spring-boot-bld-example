3f2a05730eedf364e685b893a61daa24551f2698
Erik C. Thauvin <erik@thauvin.net>
2025-08-22 21:08:55 -0700

    Bump Mockito to version 5.19.0

--- src/bld/java/com/example/springboot/ApplicationBuild.java
+++ src/bld/java/com/example/springboot/ApplicationBuild.java
@@ -31,7 +31,7 @@ public class ApplicationBuild extends WebProject {
                 .include(dependency("org.springframework.boot:spring-boot-starter-test:3.5.4"))
                 .include(dependency("org.junit.jupiter:junit-jupiter:5.13.4"))
                 .include(dependency("org.junit.platform:junit-platform-console-standalone:1.13.4"))
-                .include(dependency("org.mockito:mockito-core:5.18.0"));
+                .include(dependency("org.mockito:mockito-core:5.19.0"));
         scope(standalone)
                 .include(dependency("org.springframework.boot:spring-boot-loader:3.5.4"));
 
